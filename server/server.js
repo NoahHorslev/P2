3bc81e9de3aedca03afcb2df8a17a6c5fb40fee9
Thohard <ringwelskirobin@gmail.com>
2026-03-07 17:28:32 +0100

    server.js fix
    server.js fix + CL direct link for server.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
